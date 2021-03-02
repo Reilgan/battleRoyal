8ae84760c13c7ddad29e415ca914978391edf7a7
SkviDix <i.lyapustin@mail.ru>
2021-03-02 21:10:57 +0500

    test1 only car

---
+++ battleRoyalUnity1test/Assets/Scripts/Chat.cs
@@ -0,0 +1,46 @@
+﻿using battleRoyalServer.Common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chat : MonoBehaviour
+{
+
+    private Text uiChat;
+    private InputField uiInputField;
+    private string chat = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonClient.Instanse.OnReceiveChatMessage += OnReceiveChatMessage;
+        PhotonClient.Instanse.GetRecentChatMessage();
+        uiChat = gameObject.GetComponentInChildren<Text>();
+        uiInputField = gameObject.GetComponentInChildren<InputField>();
+        uiInputField.onEndEdit.AddListener(onEndEdit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void onEndEdit(string text)
+    {
+        PhotonClient.Instanse.SendChatMessage(text);
+        uiInputField.text = "";
+    }
+
+    private void OnReceiveChatMessage(object sender, ChatMessageEventArgs chatMessageEventArgs)
+    {
+        chat += "\n" + chatMessageEventArgs.Message;
+        uiChat.text = chat;
+    }
+
+    void onDestroy()
+    {
+        PhotonClient.Instanse.OnReceiveChatMessage -= OnReceiveChatMessage;
+    }
+}
---
+++ battleRoyalUnity1test/Assets/Scripts/Login.cs
@@ -0,0 +1,41 @@
+﻿using battleRoyalServer.Common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour
+{
+    private string Error { get; set; }
+
+    private Text LoginText;
+    private Button LoginButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoginButton =  GameObject.Find("ButtonLogin").GetComponent<Button>();
+        LoginButton.onClick.AddListener(onClickLoginButton);
+    }
+
+    void onClickLoginButton()
+    {
+        LoginText = GameObject.Find("InputLogin/Text").GetComponent<Text>();
+        string login = LoginText.text;
+        if (login == "Single")
+        {
+            PhotonClient.Instanse.loadStartScene();
+            return;
+        }
+        PhotonClient.Instanse.SendLoginOperation(login);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //PhotonClient.Instanse.SendLoginOperation(Name);
+    }
+
+}
